3e9740a1bb579dd50f8a871beca8057bc04883aa
buddhikavindra <buddhikavindra02@gmail.com>
2022-03-26 13:32:45 +0530

    changed

--- instapyway.py
+++ instapyway.py
@@ -1,13 +1,14 @@
 import instapy
 from instapy import InstaPy
 
-user = "Sebastien.peix"
-password = "0Mamyjacqueline0/"
+user = ""
+password = ""
 gecko_path = "/media/buddhi/buddhiHDD/fiver/soclose/Insta_Scrapper/geckodriver"
+userneedtoscrape = ""
 #instapy uses this internally
 session = InstaPy(username=user, password=password,geckodriver_path= gecko_path)
 session.login()
-followers = session.grab_followers(username=user,amount=11000)
+followers = session.grab_followers(username=userneedtoscrape,amount=11000)
 textfile = open('instapywayfollowers.txt', 'a+')
 
 
